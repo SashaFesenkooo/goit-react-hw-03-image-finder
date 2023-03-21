40ee0704ab5611eb98e944be7ee8f16c8ef72b00
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-03-21 18:41:19 +0100

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallety';
+
+class App extends React.Component {
+  state = {
+    imageToSearch: '',
+  };
+
+  handleSearchSubmit = imageToSearch => {
+    this.setState({ imageToSearch });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar qwe={this.handleSearchSubmit} />
+
+        <ImageGallery imageToQuery={this.state.imageToSearch} />
+
+        {/* <ToastContainer autoClose="3000" /> */}
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          limit={1}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
